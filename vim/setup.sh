410dbe9155ecec967cae0661a9098aa4b4f83cc1
int32bit <krystism@gmail.com>
2016-06-29 13:15:28 +0800

    Update vim setup

--- vim/setup.sh
+++ vim/setup.sh
@@ -1,2 +1,6 @@
 #!/bin/sh
+if [[ ! -d ~/.vim/bundle/Vundle.vim ]]; then
+    echo "Install vundle ..."
+    git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+fi
 cp -v vimrc ~/.vimrc
