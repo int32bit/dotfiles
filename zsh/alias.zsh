2249ff5e5400fa58bdcb25867462b1487821c6bd
int32bit <krystism@gmail.com>
2017-07-14 11:55:44 +0800

    Add tailf alias

--- zsh/alias.zsh
+++ zsh/alias.zsh
@@ -4,16 +4,10 @@ alias cp='cp -i'
 alias grep='grep -E'
 alias df='df -h'
 alias ag='ag --color-match "1;31"'
+alias tailf='tail -f'
 
 if which vim &>/dev/null; then
     alias vi='vim'
 fi
-
-alias harbor='docker run \
- -e HARBOR_USERNAME="admin" \
- -e HARBOR_PASSWORD="Harbor12345" \
- -e HARBOR_URL="http://192.168.56.4" \
- --net host --rm krystism/harborclient'
-alias ip='docker run --privileged -t -i --rm --network=host alpine ip'
 
 alias j='z'
